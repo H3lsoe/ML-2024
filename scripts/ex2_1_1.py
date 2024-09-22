d75af0829468702e3506dddf2e15335b344bef05
H3lsoe <morten@helsoe.dk>
2024-09-22 14:17:51 +0200

    1-0 til mig i boksekamp, men der er lang vej igen

--- scripts/ex2_1_1.py
+++ scripts/ex2_1_1.py
@@ -12,22 +12,26 @@ filename = "data/Raisin_Dataset.xls"
 doc = xlrd.open_workbook(filename).sheet_by_index(0)
 
 
-# Extract attribute names (1st row, column 4 to 12)
-attributeNames = doc.row_values(0, 3, 11)
+# Extract attribute names (1st row, column 1 to 8)
+attributeNames = doc.row_values(0, 0, 8)
+
+# print(attributeNames)
 
 # Extract class names to python list,
 # then encode with integers (dict)
-classLabels = doc.col_values(0, 2, 92)
+classLabels = doc.col_values(7, 2, 902)
 classNames = sorted(set(classLabels))
-classDict = dict(zip(classNames, range(5)))
+classDict = dict(zip(classNames, range(len(classNames))))
+
+print(classDict)
 
 # Extract vector y, convert to NumPy array
 y = np.asarray([classDict[value] for value in classLabels])
 
 # Preallocate memory, then extract excel data to matrix X
-X = np.empty((90, 8))
+X = np.empty((900, 8))
 for i, col_id in enumerate(range(3, 11)):
-    X[:, i] = np.asarray(doc.col_values(col_id, 2, 92))
+    X[:, i] = np.asarray(doc.col_values(col_id, 2, 899))
 print(len(X))
 print(len(X[0]))
 
